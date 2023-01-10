ae0dde7210483c7b068b3c94941c9ad476a26209
hanterrian <hanterrian@gmail.com>
2023-01-10 11:26:06 +0200

    Add profile

--- resources/views/profile/view.blade.php
+++ resources/views/profile/view.blade.php
@@ -1,3 +1,26 @@
 <x-layouts.app title="{{ isset($title) ? 'Profile '.$title : 'Profile Dashboard' }}">
-    {{ $slot }}
+    <main class="d-flex flex-nowrap">
+        <div class="d-flex flex-column flex-shrink-0 p-3 bg-light">
+            <ul class="nav nav-pills flex-column mb-auto">
+                <li>
+                    <a href="{{ route('profile.view') }}" class="nav-link {{ checkUrl(route('profile.view'))?'active':' link-dark' }}">
+                        Dashboard
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ route('profile.settings') }}" class="nav-link {{ checkUrl(route('profile.settings'))?'active':' link-dark' }}">
+                        Settings
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ route('profile.message') }}" class="nav-link {{ checkUrl(route('profile.message'))?'active':' link-dark' }}">
+                        Messages
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="d-flex">
+            {{ $slot }}
+        </div>
+    </main>
 </x-layouts.app>
