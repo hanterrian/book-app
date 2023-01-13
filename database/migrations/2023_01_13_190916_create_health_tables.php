5736b03335b91bdf1c45f5870cd5fe2ac640f6f7
hanterrian <hanterrian@gmail.com>
2023-01-14 00:06:25 +0200

    Fixes

--- database/migrations/2023_01_13_190916_create_health_tables.php
+++ database/migrations/2023_01_13_190916_create_health_tables.php
@@ -5,12 +5,11 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use Spatie\Health\ResultStores\EloquentHealthResultStore;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up()
     {
         $tableName = EloquentHealthResultStore::getHistoryItemInstance()->getTable();
-    
+
         Schema::create($tableName, function (Blueprint $table) {
             $table->id();
 
@@ -25,10 +24,17 @@ return new class extends Migration
 
             $table->timestamps();
         });
-        
-        Schema::table($tableName, function(Blueprint $table) {
+
+        Schema::table($tableName, function (Blueprint $table) {
             $table->index('created_at');
             $table->index('batch');
         });
+    }
+
+    public function down()
+    {
+        $tableName = EloquentHealthResultStore::getHistoryItemInstance()->getTable();
+
+        Schema::dropIfExists($tableName);
     }
 };
